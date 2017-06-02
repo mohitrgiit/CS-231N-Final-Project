dbf9d8b6c45da5a39707810664226e495a2efada
rhe23 <silentstealth23@gmail.com>
2017-06-02 01:19:22 +0000

    Edited model interface

--- src/models/config.py
+++ src/models/config.py
@@ -1,6 +1,8 @@
 class ModelConfig():
-    def __init__(self, class_size=20, image_width=128, image_height=128, image_depth=3, learning_rate=1e-3, keep_prob=1.0):
+    def __init__(self, class_size=20, batch_size=2000,
+                 image_width=128, image_height=128, image_depth=3, learning_rate=1e-3, keep_prob=1.0):
         self.class_size = class_size
+        self.batch_size = batch_size
         self.image_width = image_width
         self.image_height = image_height
         self.image_depth = image_depth
@@ -17,5 +19,4 @@ class TrainConfig():
         self.saver_address = saver_address
         self.loader_address = loader_address
         
-        self.batch_size = batch_size
         self.print_batch = print_batch
