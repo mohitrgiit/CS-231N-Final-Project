b975769b2eed469f48d7e24e4b6708243e9cf0f7
rhe23 <silentstealth23@gmail.com>
2017-06-12 16:36:04 -0700

    Put alexnet and resnet in scripts

---
+++ src/models/resnet.py
@@ -0,0 +1,187 @@
+from model import Model, lazy_property
+from multiclass_model import MulticlassModel
+
+class ResNet(Model):
+    
+    def __init__(self, model_config):
+        Model.__init__(self, model_config)
+    
+    @lazy_property
+    def prediction(self):
+        
+        conv_init = tf.layers.conv2d(
+                    inputs = self.X_placeholder,
+                    filters = 64,
+                    kernel_size = [7, 7],
+                    strides = 2,
+                    padding = "same")
+    
+        block_input = tf.layers.max_pooling2d(
+                    inputs = conv_init,
+                    pool_size = [2, 2],
+                    strides = 2)
+    
+        for idl in range(self.config.RS_NdistinctConvLayers) :
+            if (idl == 0) :
+                firstStride = 1
+            else :
+                firstStride = 2
+            
+            conv = tf.layers.conv2d(
+                        inputs = block_input,
+                        filters = self.config.RS_Nfilters[idl],
+                        kernel_size = self.config.RS_kernelSizes[0],
+                        strides = firstStride,
+                        padding = "same")
+            convBN = tf.layers.batch_normalization(
+                        conv, 
+                        training=self.is_training_placeholder)        
+            relu = tf.nn.relu(convBN)
+            residual = tf.layers.conv2d(
+                        inputs = relu,
+                        filters = self.config.RS_Nfilters[idl],
+                        kernel_size = self.config.RS_kernelSizes[1],
+                        strides = 1,
+                        padding = "same")
+            residualBN = tf.layers.batch_normalization(
+                        residual, 
+                        training=self.is_training_placeholder)
+            block_input = tf.nn.relu(residualBN)
+        
+            for ilr in range(self.config.RS_Nlayers[idl] - 1) :
+            
+                conv = tf.layers.conv2d(
+                            inputs = block_input,
+                            filters = self.config.RS_Nfilters[idl],
+                            kernel_size = self.config.RS_kernelSizes[0],
+                            strides = 1,
+                            padding = "same")
+                convBN = tf.layers.batch_normalization(
+                            conv, 
+                            training=self.is_training_placeholder)        
+                relu = tf.nn.relu(convBN)
+                residual = tf.layers.conv2d(
+                            inputs = relu,
+                            filters = self.config.RS_Nfilters[idl],
+                            kernel_size = self.config.RS_kernelSizes[1],
+                            strides = 1,
+                            padding = "same")
+                residualBN = tf.layers.batch_normalization(
+                            residual, 
+                            training=self.is_training_placeholder)        
+                output = block_input + residualBN
+                outputBN = tf.layers.batch_normalization(
+                            output, 
+                            training=self.is_training_placeholder)
+                block_input = tf.nn.relu(outputBN)
+            
+        convRes = tf.layers.batch_normalization(
+                    block_input, 
+                    training=self.is_training_placeholder)
+    
+        shape = convRes.get_shape().as_list()
+        avgPool = tf.nn.avg_pool(
+                    value=convRes, 
+                    ksize=[1,shape[1],shape[2],1],
+                    strides=[1,1,1,1],
+                    padding='VALID')
+    
+        shape = avgPool.get_shape().as_list()
+        avgPool_flat = tf.reshape(avgPool,[-1, shape[1]*shape[2]*shape[3]])
+        logits = tf.layers.dense(inputs=avgPool_flat, units=20)
+        print(logits.get_shape().as_list())
+
+        return logits
+    
+class ResNetMulticlass(MulticlassModel):
+    
+    def __init__(self, model_config):
+        MulticlassModel.__init__(self, model_config)
+    
+    @lazy_property
+    def prediction(self):
+        
+        conv_init = tf.layers.conv2d(
+                    inputs = self.X_placeholder,
+                    filters = 64,
+                    kernel_size = [7, 7],
+                    strides = 2,
+                    padding = "same")
+    
+        block_input = tf.layers.max_pooling2d(
+                    inputs = conv_init,
+                    pool_size = [2, 2],
+                    strides = 2)
+    
+        for idl in range(self.config.RS_NdistinctConvLayers) :
+            if (idl == 0) :
+                firstStride = 1
+            else :
+                firstStride = 2
+            
+            conv = tf.layers.conv2d(
+                        inputs = block_input,
+                        filters = self.config.RS_Nfilters[idl],
+                        kernel_size = self.config.RS_kernelSizes[0],
+                        strides = firstStride,
+                        padding = "same")
+            convBN = tf.layers.batch_normalization(
+                        conv, 
+                        training=self.is_training_placeholder)        
+            relu = tf.nn.relu(convBN)
+            residual = tf.layers.conv2d(
+                        inputs = relu,
+                        filters = self.config.RS_Nfilters[idl],
+                        kernel_size = self.config.RS_kernelSizes[1],
+                        strides = 1,
+                        padding = "same")
+            residualBN = tf.layers.batch_normalization(
+                        residual, 
+                        training=self.is_training_placeholder)
+            block_input = tf.nn.relu(residualBN)
+        
+            for ilr in range(self.config.RS_Nlayers[idl] - 1) :
+            
+                conv = tf.layers.conv2d(
+                            inputs = block_input,
+                            filters = self.config.RS_Nfilters[idl],
+                            kernel_size = self.config.RS_kernelSizes[0],
+                            strides = 1,
+                            padding = "same")
+                convBN = tf.layers.batch_normalization(
+                            conv, 
+                            training=self.is_training_placeholder)        
+                relu = tf.nn.relu(convBN)
+                residual = tf.layers.conv2d(
+                            inputs = relu,
+                            filters = self.config.RS_Nfilters[idl],
+                            kernel_size = self.config.RS_kernelSizes[1],
+                            strides = 1,
+                            padding = "same")
+                residualBN = tf.layers.batch_normalization(
+                            residual, 
+                            training=self.is_training_placeholder)        
+                output = block_input + residualBN
+                outputBN = tf.layers.batch_normalization(
+                            output, 
+                            training=self.is_training_placeholder)
+                block_input = tf.nn.relu(outputBN)
+            
+        convRes = tf.layers.batch_normalization(
+                    block_input, 
+                    training=self.is_training_placeholder)
+    
+        shape = convRes.get_shape().as_list()
+        avgPool = tf.nn.avg_pool(
+                    value=convRes, 
+                    ksize=[1,shape[1],shape[2],1],
+                    strides=[1,1,1,1],
+                    padding='VALID')
+    
+        shape = avgPool.get_shape().as_list()
+        avgPool_flat = tf.reshape(avgPool,[-1, shape[1]*shape[2]*shape[3]])
+
+        sbrd_logits = tf.layers.dense(inputs=avgPool_flat, units=20)
+        nsfw_logits = tf.layers.dense(inputs=avgPool_flat, units=2)
+
+        return sbrd_logits, nsfw_logits
