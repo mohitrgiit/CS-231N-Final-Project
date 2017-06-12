5b6862fe301284a48064a89160f1eb5ab4327948
rhe23 <silentstealth23@gmail.com>
2017-06-12 01:06:05 +0000

    Added task flag for confusion matrix

--- src/models/util.py
+++ src/models/util.py
@@ -147,20 +147,32 @@ def plot_confusion_matrix(cm, classes,
                           save_address = None,
                           figure_size = 11,
                           save_name = 'confusion_mat',
-                          tick_font = 10, 
-                          box_font = 10,
-                          axis_font = 10,
-                          title_font = 10,
-                          colorbar_font = 10,
-                          left_space = 10,
-                          right_space = 10,
-                          top_space = 10,
-                          bottom_space = 10):
+                          task='subreddit'):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
-    
+    if task == 'subreddit':
+        tick_font = 12 
+        box_font = 10
+        axis_font = 40
+        title_font = 50
+        colorbar_font = 20
+        left_space = 0.2
+        right_space = 1
+        top_space = 0.97
+        bottom_space = 0.05
+    elif task == 'nsfw':
+        tick_font = 50
+        box_font = 60
+        axis_font = 55
+        title_font = 58,
+        colorbar_font = 40
+        left_space = 0.25
+        right_space = 0.95
+        top_space = 0.97
+        bottom_space = 0.05
+        
     if normalize:
         cm = (cm.astype('float') / cm.sum(axis=1)[:, np.newaxis])*100
         print("Normalized confusion matrix")
